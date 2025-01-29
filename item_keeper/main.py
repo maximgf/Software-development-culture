d87ce2b3bf5a5deebd995497d26cda7d1f9f2e46
maximgf <maxim-a90@mail.ru>
2025-01-29 13:58:48 +0800

    Add metadata to /items endpoint

--- item_keeper/main.py
+++ item_keeper/main.py
@@ -8,7 +8,13 @@ def read_root():
 
 @app.get("/items")
 def get_items():
-    return {"items": ["item1", "item2", "item3"]}
+    return {
+        "items": [
+            {"id": 1, "name": "item1", "description": "A fancy item", "price": 10.99},
+            {"id": 2, "name": "item2", "description": "A useful item", "price": 5.49},
+            {"id": 3, "name": "item3", "description": "A rare item", "price": 99.99},
+        ]
+    }
 
 @app.get("/items_by_id")
 def get_items_by_id():
