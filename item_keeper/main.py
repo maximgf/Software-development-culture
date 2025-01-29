4af366c584f8f4e8b1cd9678f7928a5cd0b0ff97
nnmaxim <rogozhnikov7474@mail.ru>
2025-01-29 14:03:12 +0800

    Add pagination to /items endpoint

--- item_keeper/main.py
+++ item_keeper/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Query
 
 app = FastAPI()
 
@@ -7,8 +8,9 @@ def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/items")
-def get_items():
-    return {"items": ["item1", "item2", "item3"]}
+def get_items(skip: int = Query(0), limit: int = Query(10)):
+    all_items = ["item1", "item2", "item3", "item4", "item5"]
+    return {"items": all_items[skip : skip + limit]}
 
 @app.get("/items_by_id")
 def get_items_by_id():
